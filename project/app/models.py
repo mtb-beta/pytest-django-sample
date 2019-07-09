208aa0f775710c13d83a4b1023179f685255fdc0
mtb_beta <mtb.toya0403@gmail.com>
2019-07-09 17:39:50 +0700

    add test is_empty

--- project/app/models.py
+++ project/app/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class MyAppModel(models.Model):
     title = models.CharField(max_length=20)
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=255, default="")
 
+    @property
+    def is_empty(self):
+        return bool(self.content)
--- project/app/tests.py
+++ project/app/tests.py
@@ -1,2 +1,13 @@
-def test_first_case():
-    assert True
+import pytest
+
+from app.models import MyAppModel
+
+@pytest.mark.django_db
+def test_myapp_is_empty():
+    mymodel = MyAppModel(title="sample model", content="test")
+    assert mymodel.is_empty
+
+@pytest.mark.django_db
+def test_myapp_isnot_emmpty():
+    mymodel = MyAppModel(title="sample model")
+    assert not mymodel.is_empty
