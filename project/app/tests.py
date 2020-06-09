b764ff41853e7b062966281b4e5fc74a018d36df
mtb-beta <mtb.toya0403@gmail.com>
2020-06-09 10:28:20 +0900

    add view test

--- project/app/tests.py
+++ project/app/tests.py
@@ -11,3 +11,8 @@ def test_myapp_is_empty():
 def test_myapp_isnot_emmpty():
     mymodel = MyAppModel(title="sample model")
     assert not mymodel.is_empty
+
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
