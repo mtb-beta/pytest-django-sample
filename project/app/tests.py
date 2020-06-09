82d4569cfcc92e1548093d187cdf65ff9c9e7424
mtb-beta <mtb.toya0403@gmail.com>
2020-06-09 10:33:38 +0900

    add sample pytest.mark.parametrize

--- project/app/tests.py
+++ project/app/tests.py
@@ -2,10 +2,12 @@ import pytest
 
 from app.models import MyAppModel
 
+
 @pytest.mark.django_db
 def test_myapp_is_empty():
     mymodel = MyAppModel(title="sample model", content="test")
     assert mymodel.is_empty
+
 
 @pytest.mark.django_db
 def test_myapp_isnot_emmpty():
@@ -14,5 +16,11 @@ def test_myapp_isnot_emmpty():
 
 
 def test_hello(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize("path, status_code", (["/", 200], ["", 200], ["hello", 404],))
+def test_url_response(client, path, status_code):
+    response = client.get(path)
+    assert response.status_code == status_code
